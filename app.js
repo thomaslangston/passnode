db26d211df178a435617029fde0e3aabdb2a2831
Thomas Langston <thomas.langston@gmail.com>
2014-05-11 20:09:54 -0400

    cleanup

--- app.js
+++ app.js
@@ -6,8 +6,10 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var flash = require('connect-flash');
-var fs = require("fs");
-var sqlite3 = require("sqlite3").verbose();
+var fs = require('fs');
+var sqlite3 = require('sqlite3').verbose();
+var passport = require('passport');
+var passportLocalStrat = require('passport-local').Strategy;
 
 var file = "test.db"
 var exists = fs.existsSync(file);
@@ -32,19 +34,21 @@ app.use(bodyParser.urlencoded());
 app.use(cookieParser('a not so secret secret'));
 app.use(express.session({ cookie: { maxAge: 60000 }}));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(app.router);
 app.use(flash());
 
 var routes = require('./routes')(app);
 
-/// catch 404 and forwarding to error handler
+// catch 404 and forwarding to error handler
 app.use(function(req, res, next) {
     var err = new Error('Not Found');
     err.status = 404;
     next(err);
 });
 
-/// error handlers
+// error handlers
 
 // development error handler
 // will print stacktrace
