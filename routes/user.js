3b2f06114c5e7758e495e096e3523fcaf5e55279
Thomas Langston <thomas.langston@gmail.com>
2014-05-18 15:05:24 -0400

    Create a user lsiting view

--- routes/user.js
+++ routes/user.js
@@ -1,4 +1,14 @@
 /* GET users listing. */
 exports.list = function(req, res){
-  res.send('respond with a resource');
+    //res.send('respond with a resource');
+    
+    //initialize db
+    //call statement
+    var users = [
+        {email: 'thomas.langston@example.com', password: 'password'},
+        {email:'john.smith@example.org',password: 'ilovejonathoncoulton'}
+    ];
+    //close db
+
+  res.render('users', { title: 'Users', users: users } );
 };
